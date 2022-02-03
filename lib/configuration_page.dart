c6f0400459df098e8944f7e485149b6383043d6e
Ivan Vasilev <ivan@zmeiresearch.bg>
2022-02-03 12:55:40 +0200

    Add padding to configuration page elements

--- lib/configuration_page.dart
+++ lib/configuration_page.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-import 'slider_group.dart'
+import 'slider_group.dart';
 
-class AppMainPage extends StatelessWidget {
+class ConfigurationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PlatformScaffold(
-        appBar: PlatformAppBar(
-          title: Text('zWiggler config'),
-        ),
-        body: ListView(children: [
-          SliderGroup(
-            title: "Test Slider: ",
-            initialValue: 10,
-            minValue: 5,
-            maxValue: 20,
-            divisions: 3,
-          ),
-        ]));
+    return Column(
+      children: const [
+        Padding(
+            padding: EdgeInsets.all(16.0),
+            child: SliderGroup(
+                title: "Activity Time (min)",
+                initialValue: 20,
+                minValue: 5,
+                maxValue: 1500)),
+        Padding(
+            padding: EdgeInsets.all(16.0),
+            child: SliderGroup(
+                title: "Movement period (sec)",
+                initialValue: 60,
+                minValue: 10,
+                maxValue: 300)),
+      ],
+    );
   }
 }
